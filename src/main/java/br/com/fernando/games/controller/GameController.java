a1949e981a2e4d61e884289879e200f263dede04
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-05-04 16:15:13 -0300

    Adding DELETE method for game

--- src/main/java/br/com/fernando/games/controller/GameController.java
+++ src/main/java/br/com/fernando/games/controller/GameController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +28,7 @@ public class GameController {
 	
 	@PostMapping
 	@Transactional
-	public ResponseEntity cadastrar(@RequestBody @Valid DataRegistrationGame dataRegistrationGame, UriComponentsBuilder uriBuilder){
+	public ResponseEntity register(@RequestBody @Valid DataRegistrationGame dataRegistrationGame, UriComponentsBuilder uriBuilder){
 		var game = new Game(dataRegistrationGame);
 		gamesRepository.save(game);
 		
@@ -36,10 +38,18 @@ public class GameController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<Game>> listar(){
+	public ResponseEntity<List<Game>> list(){
 		List<Game> games = gamesRepository.findAll();
 		
 		return ResponseEntity.ok(games);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity delete(@PathVariable Long id) {
+		var game = gamesRepository.getReferenceById(id);
+		gamesRepository.delete(game);
+		return ResponseEntity.noContent().build();
 	}
 
 }
