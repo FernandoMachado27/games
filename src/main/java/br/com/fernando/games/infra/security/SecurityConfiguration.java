9f2f5f2fb9b2bc467f0e3fd3b9b0c49a4c67e16f
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-05-11 00:16:45 -0300

    Adding SecurityConfiguration

---
+++ src/main/java/br/com/fernando/games/infra/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package br.com.fernando.games.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	private SecurityFilter securityFilter;
+
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+		return http.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().authorizeHttpRequests()
+				.requestMatchers(HttpMethod.POST, "/login").permitAll()
+				.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+				.anyRequest().authenticated()
+				.and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
+
+}
